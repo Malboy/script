5ebb323d92feea3a5bef9446f25398b64748be53
Bor_on <borzyhanton@gmail.com>
2023-07-31 02:11:47 +0300

    les1

---
+++ lesson1/script.js
@@ -0,0 +1,26 @@
+console.log('Текст из script.js');
+const num = 123;
+var a = 5;
+a = 7;
+console.log(a); //asagagf
+var b = 1 + 2 + 3;
+console.log(b);
+/*text 2*/
+var c = 10;
+var d = 2;
+sumCD = c + d;
+subCD = c - d;
+multCD = c * d;
+delCD = c / d;
+console.log(`сумма чисел c и d = ${sumCD},
+разность = ${subCD}, 
+произведение = ${multCD}, 
+частное ${delCD}`);
+console.log(`Общая сумма этих чисел = ${sumCD + subCD + multCD + delCD}`);
+
+
+
+var tempCelc = 23;
+const tempFar = (9 / 5) * tempCelc + 32;
+console.log(`Температура в Цельсиях: ${tempCelc};
+Температура в Фаренгейтах: ${tempFar};`);
